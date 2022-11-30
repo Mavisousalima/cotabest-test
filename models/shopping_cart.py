9160b220bc9066e77039e23a226c4dd2a16892bc
Mavisousalima <mavisousalima@gmail.com>
2022-11-30 14:51:43 -0300

    fix: Add minimun and max_availability verification when adding a product to cart

--- models/shopping_cart.py
+++ models/shopping_cart.py
@@ -51,13 +51,20 @@ class ShoppingCart(Base):
 
     @staticmethod
     def add_product_to_shopping_cart(shopping_cart_id: int, product_id: int, amount: int, db: Session):
-        shopping_cart = db.query(ShoppingCart).filter(
-            ShoppingCart.id == shopping_cart_id).first()
+        shopping_cart = db.query(ShoppingCart).filter(ShoppingCart.id == shopping_cart_id).first()
         if not shopping_cart:
             shopping_cart = ShoppingCart.create_shopping_cart(amount, db)
+
         product = db.query(Product).filter(Product.id == product_id).first()
         if not product:
             raise HTTPException(status_code=404, detail="Product not found")
+
+        if product.minimun > amount:
+            raise HTTPException(status_code=400, detail=f"Minimum quantity for this product: {product.minimun}")
+
+        if product.max_availability < amount:
+            raise HTTPException(status_code=400, detail=f"Desired quantity exceeds available quantity. MAX: {product.max_availability}")
+
         order = Order.create_order(shopping_cart.id, product.id, db)
         shopping_cart.orders.append(order)
         db.commit()
--- tests/test_shopping_cart.py
+++ tests/test_shopping_cart.py
@@ -2,6 +2,7 @@ import json
 import pytest
 
 from models.shopping_cart import ShoppingCart
+from models.product import Product
 
 
 def test_add_product_to_shopping_cart(test_app, monkeypatch):
@@ -21,6 +22,38 @@ def test_add_product_to_shopping_cart(test_app, monkeypatch):
         "message": "Product added to shopping cart successfully"
     }
 
+"""def test_product_minimun_quantity(test_app, monkeypatch):
+    product_payload = {
+        "id": 1,
+        "name": "Ração para cachorro",
+        "price": 50.0,
+        "minimun": 10,
+        "amount_per_package": 2,
+        "max_availability": 50000
+    }
+
+    def mock_post(db_session, name, price, minimun, amount_per_package, max_availability):
+        return product_payload
+
+    monkeypatch.setattr(Product, "create_product", mock_post)
+
+    response = test_app.post("/product/create", data=json.dumps(product_payload))
+
+    assert response.status_code == 201
+
+    cart_payload = {
+        "id": 1,
+        "amount": 2
+    }
+
+    def mock_post(db_session, shopping_cart_id, id, amount):
+        return cart_payload
+
+    monkeypatch.setattr(ShoppingCart, "add_product_to_shopping_cart", mock_post)
+
+    response = test_app.post("/shopping-cart/add-to-cart?shopping_cart_id=1", data=json.dumps(cart_payload))
+    assert response.status_code == 400
+    assert response.json() == {"message": "Minimum quantity for this product: 10"}"""
 
 def test_get_current_shopping_cart(test_app, monkeypatch):
     cart_response = {
@@ -41,13 +74,3 @@ def test_get_current_shopping_cart(test_app, monkeypatch):
         'shopping_cart_id': 1,
         'product_id': 1,
     }
-
-"""def test_remove_product_from_shopping_cart(test_app, monkeypatch):
-    response = test_app.post(
-        "/product/remove-from-cart",
-        json={"id": 1},
-    )
-    assert response.status_code == 200
-    assert response.json() == {
-        "message": "Product removed from shopping cart successfully"
-    }"""
